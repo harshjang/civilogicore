fd1062c6076f22e9bbf323059ec9490dba275fda
harshjang <harshjangra870@gmail.com>
2026-03-17 12:28:28 +0530

    fix render deployment

--- server/server.js
+++ server/server.js
@@ -1,15 +1,14 @@
-import WebSocket, { WebSocketServer } from "ws";
+const WebSocket = require("ws");
 
 const port = process.env.PORT || 3000;
 
-const wss = new WebSocketServer({ port });
+const server = new WebSocket.Server({ port });
 
 console.log("WebSocket server running on port", port);
 
-wss.on("connection", (ws) => {
+server.on("connection", (ws) => {
   console.log("Client connected");
 
-  // Example: send dummy survey point every 3 sec
   const interval = setInterval(() => {
     const point = `${Math.random()*100},${Math.random()*100},${Math.random()*50}`;
     ws.send(point);
